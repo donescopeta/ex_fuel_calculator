3c50897f23fe3088483534ca9fcf14d6b1db9f10
donescopeta <mateusz.sokolowski@appunite.com>
2022-05-05 19:41:18 +0200

    fix: typespecs

--- lib/compute_path.ex
+++ lib/compute_path.ex
@@ -2,24 +2,23 @@ defmodule FuelCalculator.FlightPath do
   alias FuelCalculator.ComputeLaunchFuel
   alias FuelCalculator.ComputeLandingFuel
 
-  @spec compute_path(float() | integer(), list(([:atom, float()])) :: float()
+  @spec call(float() | integer(), list()) :: float()
   def call(mass, path) do
     compute_path(mass, path) - mass
   end
 
-  @spec compute(float() | integer(), [:land, float()]) :: float()
+  @spec compute(float() | integer(), list()) :: float()
   def compute(mass, [:land, gravity]) do
     ComputeLandingFuel.call(mass, gravity)
   end
 
-  @spec compute(float() | integer(), [:launch, float()]) :: float()
   def compute(mass, [:launch, gravity]) do
     ComputeLaunchFuel.call(mass, gravity)
   end
 
   def compute_path(mass, []), do: mass
 
-  @spec compute_path(float() | integer(), list(([:atom, float()])) :: float()
+  @spec compute_path(float() | integer(), list()) :: float()
   def compute_path(mass, path) do
     {step, path_remaining} = List.pop_at(path, -1)
 
