91316ea1cca771f20f5ed12ecb33f696111b3ed8
donescopeta <mateusz.sokolowski@appunite.com>
2022-05-05 19:45:33 +0200

    fix: list to tuples

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 
 ```
 iex -S mix
-FuelCalculator.compute_path(14606, [[:launch, 9.807], [:land, 3.711], [:launch, 3.711], [:land, 9.807]])
+FuelCalculator.compute_path(14606, [{:launch, 9.807}, {:land, 3.711}, {:launch, 3.711}, {:land, 9.807}])
 ```
--- lib/compute_path.ex
+++ lib/compute_path.ex
@@ -2,23 +2,24 @@ defmodule FuelCalculator.FlightPath do
   alias FuelCalculator.ComputeLaunchFuel
   alias FuelCalculator.ComputeLandingFuel
 
-  @spec call(float() | integer(), list()) :: float()
+  @spec call(float() | integer(), [{:atom, float()}]) :: float()
   def call(mass, path) do
     compute_path(mass, path) - mass
   end
 
-  @spec compute(float() | integer(), list()) :: float()
-  def compute(mass, [:land, gravity]) do
+  @spec compute(float() | integer(), {:land, float()}) :: float()
+  def compute(mass, {:land, gravity}) do
     ComputeLandingFuel.call(mass, gravity)
   end
 
-  def compute(mass, [:launch, gravity]) do
+  @spec compute(float() | integer(), {:launch, float()}) :: float()
+  def compute(mass, {:launch, gravity}) do
     ComputeLaunchFuel.call(mass, gravity)
   end
 
   def compute_path(mass, []), do: mass
 
-  @spec compute_path(float() | integer(), list()) :: float()
+  @spec compute_path(float() | integer(), list({:atom, float()})) :: float()
   def compute_path(mass, path) do
     {step, path_remaining} = List.pop_at(path, -1)
 
